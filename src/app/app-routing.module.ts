92dba0e6a5f2364e4ca70e61626e85e1705bffdf
Iñaki <goicoecheagabriel@gmail.com>
2021-09-04 23:05:38 +0200

    Fin de la seccion 15. Y fin de la aplicacion de Heroes. En esta seccion rRealizamos un control de acceso a una ruta utilizando el guards para controlar por medio de canLoad y canActivate si el usuario puede o no acceder a una ruta determinada.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import { AuthGuard } from './auth/guards/auth.guard';
 
 const routes: Routes = [
   {
-    path: 'auth', loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule )
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule )
   },
   {
-    path: 'heroes', loadChildren: () => import('./heroes/heroes.module').then( m => m.HeroesModule )
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then( m => m.HeroesModule ),
+    canLoad: [ AuthGuard ],
+    canActivate: [ AuthGuard ]
   },
   {
     path: 'store', loadChildren: () => import('./store/store.module').then( m => m.StoreModule )
--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth.service';
+import { Auth } from '../../../auth/interfaces/auth.interface';
 
 @Component({
   selector: 'app-home',
@@ -11,11 +14,19 @@ import { Component, OnInit } from '@angular/core';
     `
   ]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get auth() {
+    return this.authService.auth
   }
+
+  constructor( private router: Router,
+               private authService: AuthService ) { }
+
+ logout(){
+  localStorage.removeItem('token');
+  this.router.navigate(['./auth'])
+
+ }
 
 }
